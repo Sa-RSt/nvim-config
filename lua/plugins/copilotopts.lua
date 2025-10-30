3e7a3e52d8d5f1497f6c4fc2b09d177d690be5cf
Sa-RSt <sasha.rodelasteidle@gmail.com>
2025-10-29 22:42:56 -0300

    2025-10-29

--- lua/plugins/copilotopts.lua
+++ lua/plugins/copilotopts.lua
@@ -1,37 +1,8 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "BufReadPost",
-  dependencies = {
-    "copilotlsp-nvim/copilot-lsp",
-  },
+  "kiddos/gemini.nvim",
   opts = {
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      hide_during_completion = false,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
-      },
+    completion = {
+      completion_delay = 1500,
     },
-    nes = {
-      enabled = false,
-      keymap = {
-        accept_and_goto = "<leader>p",
-        accept = false,
-        dismiss = "<Esc>",
-      },
-    },
-    panel = { enabled = true },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
-  keys = {
-    { "<leader>cx", ":Copilot panel<cr>", desc = "Copilot Panel" },
   },
 }
