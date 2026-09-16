0ef941e03df649cfc4f6bbb11ddd4f54ea5ba405
Sa-RSt <sasha.rodelasteidle@gmail.com>
2026-09-15 23:31:13 -0300

    Configure verible LSP

--- init.lua
+++ init.lua
@@ -1,4 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.g.python3_host_prog = vim.fn.expand("~/nvim-python/bin/python3")
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  -- delay update diagnostics
+  update_in_insert = false,
+})
+-- require("lspconfig").verible.setup({
+--   -- cmd = { 'verible-verilog-ls', '--rules_config_search' },
+--   cmd = { "verible-verilog-ls" },
+--   root_dir = require("lspconfig").util.root_pattern({ ".git", "verilator.f" }),
+--   -- capabilities = capabilities,
+--   format_on_save = true,
+-- })
 require("config.lazy")
 vim.cmd("runtime! plugin/rplugin.vim")
