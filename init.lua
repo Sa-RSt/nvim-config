bf62f59af33a06919e6d82e4fa3ea0e5c2c9bf6c
Sa-RSt <sasha.rodelasteidle@gmail.com>
2026-09-15 23:29:19 -0300

    Change python venv

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 vim.g.python3_host_prog = vim.fn.expand("~/nvim-python/bin/python3")
 require("config.lazy")
 vim.cmd("runtime! plugin/rplugin.vim")
